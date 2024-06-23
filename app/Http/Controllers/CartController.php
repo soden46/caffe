228176d42402a374031769a884ccf2ed5b4ac0d2
Soden46 <syarifsoden22@gmail.com>
2024-06-23 19:36:47 +0700

    Update

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Menu;
-use Darryldecode\Cart\Cart;
+use Darryldecode\Cart\Facades\CartFacade as Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
     public function index()
     {
         $userId = auth()->user()->id;
-        $cartContent = \Cart::session($userId)->getContent();
-        return view('Cart.index')->with([
+        $cartContent = Cart::session($userId)->getContent();
+
+        return view('cart.index')->with([
             'items' => $cartContent,
             'itemsCount' => $cartContent->count(), // qte
         ]);
@@ -21,38 +21,48 @@ class CartController extends Controller
 
     public function addMenuToCart(Request $request, $id)
     {
-
         $menu = Menu::findOrFail($id);
-        // dd($menu);
         $userId = auth()->user()->id;
-        \Cart::session($userId)->add(array(
+
+        Cart::session($userId)->add(array(
             'id' => $menu->id,
-            'name' => $menu->title,
-            'price' => $menu->pric,
-            "quantity" => $request->quantity ? $request->quantity : 1, // ila jat quatity idkhlha ila majatch i3tiha 1
-            "attributes" => array(),
+            'name' => $menu->judul, // Jika field judul di database adalah 'judul'
+            'price' => $menu->harga, // Jika field harga di database adalah 'harga'
+            "quantity" => $request->quantity ? $request->quantity : 1, // Jika quantity tidak ada, default 1
+            "attributes" => array(
+                'foto' => $menu->foto,
+                'deskripsi' => $menu->deskripsi,
+                'harga_lama' => $menu->harga_lama,
+                'id_kategori' => $menu->id_kategori,
+            ),
             "associatedModel" => $menu,
         ));
+
         return redirect()->route('cart.index');
     }
-    // update item in cart fuction
+
+    // update item in cart function
     public function updateItemInCart($id, Request $request)
     {
         $userId = auth()->user()->id;
-        \Cart::session($userId)->update($id, array(
+
+        Cart::session($userId)->update($id, array(
             'quantity' => array(
                 'relative' => false,
                 'value' => $request->quantity,
-            )
+            ),
         ));
+
         return redirect()->route('cart.index');
     }
 
-    // remove item from cart fuction
+    // remove item from cart function
     public function removeItemFromCart($id)
     {
         $userId = auth()->user()->id;
-        \Cart::session($userId)->remove($id);
+
+        Cart::session($userId)->remove($id);
+
         return redirect()->route('cart.index');
     }
 }
--- resources/views/Cart/index.blade.php
+++ resources/views/Cart/index.blade.php
@@ -1,81 +1,79 @@
-
 @extends('layout.app')
 
 @section('content')
     <!-- cart item -->
     <div class="small-container cart-page mb-4">
         <table>
-            <tr>
-                <th>Menu</th>
-                <th>Quantity</th>
-                <th>total</th>
-            </tr>
-
+            <thead>
+                <tr>
+                    <th>Menu</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
+                </tr>
+            </thead>
+            <tbody>
                 @foreach ($items as $item)
-                     <tr>
+                    <tr>
                         <td>
                             <div class="cart-info">
-                                <img src="{{asset('images/menu/'.$item->associatedModel->image)}}" alt="">
+                                <img src="{{ asset('images/menu/' . $item->attributes->foto) }}" alt="{{ $item->name }}">
                                 <div>
-                                    <p>{{$item->name}}</p>
-                                    <small>Price : {{$item->price}} MAD</small>
-                                </br>
-                                        {{-- remove menu from cart form --}}
-                                        <form action="{{route('cart.remove',$item->id)}}"
-                                                    method="POST"
-                                                    id="removeItemform">
-                                            @csrf
-                                            @method("DELETE")
-                                            <button class="removeBtn" type="submit">Remove</button>
-                                        </form>
+                                    <p>{{ $item->name }}</p>
+                                    <small>Harga: Rp. {{ number_format($item->price, 2) }}</small>
+                                    <br>
+                                    <form action="{{ route('cart.remove', $item->id) }}" method="POST" id="removeItemForm">
+                                        @csrf
+                                        @method("DELETE")
+                                        <button class="removeBtn" type="submit">Remove</button>
+                                    </form>
                                 </div>
                             </div>
                         </td>
                         <td>
-                            <form action="{{route('cart.update',$item->id)}}" method="POST" >
+                            <form action="{{ route('cart.update', $item->id) }}" method="POST">
                                 @csrf
                                 @method('PUT')
                                 <input type="number"
-                                    name="quantity"
-                                    id="quantity"
-                                    value="{{$item->quantity}}"
-                                    min="1"
-                                    max="{{$item->associatedModel->quantity}}"
-                                 >
-                                <button type="submit" class="btn btn-sm btn-warning">
-                                    <i class="fas fa fa-edit"></i>
+                                       name="quantity"
+                                       id="quantity"
+                                       value="{{ $item->quantity }}"
+                                       min="1"
+                                       max="{{ $item->attributes->quantity }}"
+                                       class="form-control">
+                                <button type="submit" class="btn btn-sm btn-warning mt-2">
+                                    <i class="fas fa-edit"></i> Update
                                 </button>
                             </form>
-                            </td>
-                        <td class="price">{{$item->quantity * $item->price }} MAD</td>
-
-
+                        </td>
+                        <td class="price">Rp. {{ number_format($item->quantity * $item->price, 2) }}</td>
                     </tr>
                 @endforeach
+            </tbody>
         </table>
-        @if (Cart::getSubTotal()>0)
-            <div class="total-price">
-            <table>
-                <tr>
-                    <td>SubTotal</td>
-                    <td class="fw-bolder">{{Cart::getSubTotal() }} MAD</td>
-                </tr>
-                 <tr>
-                    <td  colspan="2">
-                        @if (Cart::getSubTotal()>0)
-                                {{-- payment using paypal --}}
+        @if (Cart::getSubTotal() > 0)
+            <div class="total-price mt-4">
+                <table>
+                    <tr>
+                        <td>SubTotal</td>
+                        <td class="fw-bolder">Rp. {{ number_format(Cart::getSubTotal(), 2) }}</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            @if (Cart::getSubTotal() > 0)
                                 <div class="row">
                                     <div class="form-group">
-                                        <a href="{{route('make.payment')}}" class=" btn-paypal mt-3 ml-2 d-flex align-items-center "  >
-                                            <i class="fab fa-cc-paypal mr-1" style="font-size: 1.7rem"></i>   PAY  {{Cart::getSubTotal() }} MAD WITH PAYPAL
+                                        <a href="{{ route('make.payment') }}" class="btn-paypal mt-3 ml-2 d-flex align-items-center">
+                                            <i class="fab fa-cc-paypal mr-1" style="font-size: 1.7rem"></i>
+                                            PAY Rp. {{ number_format(Cart::getSubTotal(), 2) }} WITH PAYPAL
                                         </a>
                                     </div>
-                        @endif
-                    </td>
-                </tr>
-            </table>
-        </div>
+                                </div>
+                            @endif
+                        </td>
+                    </tr>
+                </table>
+            </div>
         @endif
-
+        </div>
     </div>
 @endsection
